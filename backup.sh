cb63617b2f12ef37640e96d1ac8c7ddd16385238
Adailton Silva <adailton.silva10@live.com>
2019-05-10 11:35:33 -0300

    Arquivos de backup e docker-compose
    docker-compose.yaml: é o arquivo responsável por criar os containers docker que rodam o banco de dados Postgres, Pgadmin e Jenkins

---
+++ backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+
+#Criando uma variavel para receber o mesmo tipo de nome do dump
+dump=dump_`date +%d-%m-%Y_%M`.sql
+
+cd /home/centos/docker-compose
+
+#Acessando o container e executando o comando "pg_dumpall" que cria o dump
+/bin/docker exec -t db pg_dump maquinaTest -U postgres  --format=p --no-owner --no-privileges --no-tablespaces --no-unlogged-table-data --inserts > $dump
+
+#Movendo qualquer arquivo do tipo .sql para a pasta Dump-Postgres(repositoio github)
+mv *sql dump-postgres/
+
+#Acessando a pasta repositorio
+cd dump-postgres/
+
+echo "GIT PULL"
+git pull
+
+echo "GIT ADD"
+git add $dump
+
+echo "GIT COMMIT"
+git commit -m "Dump semanal : "$dump
+
+echo "GIT PUSH"
+git push origin master
+
+rm $dump
