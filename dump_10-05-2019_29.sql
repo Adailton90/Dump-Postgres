3a669b45859a921fea983663b0277cdb39b15def
aice10 <adailton.silva10@live.com>
2019-05-10 01:29:05 +0000

    Dump semanal : dump_10-05-2019_29.sql

--- dump_10-05-2019_29.sql
+++ dump_10-05-2019_29.sql
@@ -1,119 +1,4 @@
 --
--- PostgreSQL database cluster dump
---
-
-SET default_transaction_read_only = off;
-
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-
---
--- Drop databases (except postgres and template1)
---
-
-DROP DATABASE "maquinaTest";
-
-
-
-
---
--- Drop roles
---
-
-DROP ROLE postgres;
-
-
---
--- Roles
---
-
-CREATE ROLE postgres;
-ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS PASSWORD 'md524bb002702969490e41e26e1a454036c';
-
-
-
-
-
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 11.2 (Debian 11.2-1.pgdg90+1)
--- Dumped by pg_dump version 11.2 (Debian 11.2-1.pgdg90+1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
-UPDATE pg_catalog.pg_database SET datistemplate = false WHERE datname = 'template1';
-DROP DATABASE template1;
---
--- Name: template1; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
-
-
-ALTER DATABASE template1 OWNER TO postgres;
-
-\connect template1
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: DATABASE template1; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON DATABASE template1 IS 'default template for new databases';
-
-
---
--- Name: template1; Type: DATABASE PROPERTIES; Schema: -; Owner: postgres
---
-
-ALTER DATABASE template1 IS_TEMPLATE = true;
-
-
-\connect template1
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: DATABASE template1; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE CONNECT,TEMPORARY ON DATABASE template1 FROM PUBLIC;
-GRANT CONNECT ON DATABASE template1 TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
-
---
 -- PostgreSQL database dump
 --
 
@@ -131,46 +16,23 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: maquinaTest; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE "maquinaTest" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
-
-
-ALTER DATABASE "maquinaTest" OWNER TO postgres;
-
-\connect "maquinaTest"
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: unaccent; Type: EXTENSION; Schema: -; Owner: 
+-- Name: unaccent; Type: EXTENSION; Schema: -; Owner: -
 --
 
 CREATE EXTENSION IF NOT EXISTS unaccent WITH SCHEMA public;
 
 
 --
--- Name: EXTENSION unaccent; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION unaccent; Type: COMMENT; Schema: -; Owner: -
 --
 
 COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';
 
 
-SET default_tablespace = '';
-
 SET default_with_oids = false;
 
 --
--- Name: tb_versao_corte; Type: TABLE; Schema: public; Owner: postgres
+-- Name: tb_versao_corte; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.tb_versao_corte (
@@ -181,10 +43,8 @@ CREATE TABLE public.tb_versao_corte (
 );
 
 
-ALTER TABLE public.tb_versao_corte OWNER TO postgres;
-
 --
--- Name: tb_versao_corte_id_versao_corte_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: tb_versao_corte_id_versao_corte_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.tb_versao_corte_id_versao_corte_seq
@@ -196,41 +56,38 @@ CREATE SEQUENCE public.tb_versao_corte_id_versao_corte_seq
     CACHE 1;
 
 
-ALTER TABLE public.tb_versao_corte_id_versao_corte_seq OWNER TO postgres;
-
 --
--- Name: tb_versao_corte_id_versao_corte_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: tb_versao_corte_id_versao_corte_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.tb_versao_corte_id_versao_corte_seq OWNED BY public.tb_versao_corte.id_versao_corte;
 
 
 --
--- Name: tb_versao_corte id_versao_corte; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: tb_versao_corte id_versao_corte; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.tb_versao_corte ALTER COLUMN id_versao_corte SET DEFAULT nextval('public.tb_versao_corte_id_versao_corte_seq'::regclass);
 
 
 --
--- Data for Name: tb_versao_corte; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: tb_versao_corte; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.tb_versao_corte (id_versao_corte, plataforma, numero_versao, nome_versao) FROM stdin;
-1	Android	29	2.0.0
-2	IOS	1	1.0.0
-\.
-
-
---
--- Name: tb_versao_corte_id_versao_corte_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.tb_versao_corte_id_versao_corte_seq', 2, true);
+INSERT INTO public.tb_versao_corte VALUES (1, 'Android', 29, '2.0.0');
+INSERT INTO public.tb_versao_corte VALUES (2, 'Atualiza do IOS', 1, '1.0.0');
+INSERT INTO public.tb_versao_corte VALUES (3, 'JEferson', 1, '3.0.0');
 
 
 --
--- Name: tb_versao_corte pk_tb_versao_corte; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: tb_versao_corte_id_versao_corte_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.tb_versao_corte_id_versao_corte_seq', 3, true);
+
+
+--
+-- Name: tb_versao_corte pk_tb_versao_corte; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.tb_versao_corte
@@ -239,59 +96,5 @@ ALTER TABLE ONLY public.tb_versao_corte
 
 --
 -- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 11.2 (Debian 11.2-1.pgdg90+1)
--- Dumped by pg_dump version 11.2 (Debian 11.2-1.pgdg90+1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE postgres;
---
--- Name: postgres; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE postgres WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
-
-
-ALTER DATABASE postgres OWNER TO postgres;
-
-\connect postgres
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
---
-
-COMMENT ON DATABASE postgres IS 'default administrative connection database';
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database cluster dump complete
 --
 
